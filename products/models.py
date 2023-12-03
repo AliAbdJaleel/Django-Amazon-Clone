b3d42a7497adbc2c024aa19f03ab8d151128c67d
AliAbdJaleel <aliabdjaleel85@gmail.com>
2023-12-04 01:06:22 +0300

    design models for app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from taggit.managers import TaggableManager # pip install django-taggit
+from django.utils import timezone
 # Create your models here.
+
+FLAG_TYPE = (
+
+    ('NEW','NEW'),
+    ('Sale','Sale'),
+    ('Feature','Feature')
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=120)
+    flag = models.CharField(max_length=10,choices=FLAG_TYPE)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='product') # defult image that show on view product
+    sku = models.IntegerField()
+    subtitle = models.TextField(max_length=500)
+    description = models.TextField(max_length=50000)
+    tags = TaggableManager()
+    brand = models.ForeignKey('Brand',related_name='product_brand',on_delete=models.SET_NULL = True)
+
+
+
+class ProductImages(models.Model):
+    product = models.ForeignKey(Product,related_name='product_image',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='productimages') # productimages is the Name of folder contains images
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='brand')
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, related_name= 'review_user',on_delete=models.SET_NULL,null=True)
+    product = models.ForeignKey(Product,related_name='review_product',on_delete=models.CASCADE)
+    review = models.TextField(max_length=500)
+    rate = models.IntegerField(choices= [(i,i) for i in range(1,6)])
+    created_at = models.DateTimeField(default=timezone.now)                        
