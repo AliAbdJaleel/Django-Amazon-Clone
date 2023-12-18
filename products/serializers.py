04b7a0dfaa657fc9846733346a0c76895d8e203e
علي قيس عبد الجليل <142967310+AliAbdJaleel@users.noreply.github.com>
2023-12-18 13:26:02 +0300

    show bramd name in product list Api

--- products/serializers.py
+++ products/serializers.py
@@ -3,6 +3,7 @@ from .models import Product , Brand
 
 
 class ProductListSerializer(serializers.ModelSerializer):
+    brand = serializers.StringRelatedField()  # هذا السطر لغرض استرجاع الاسم المذكور في دالة الاس تي ار الموجودة في المودل بدلا من الاي دي
     class Meta:
         model = Product
         fields = '__all__'
