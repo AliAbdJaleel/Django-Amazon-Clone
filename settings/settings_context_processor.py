61d4bbc51f0967e394cb9f2e53c8bf603baf760a
علي قيس عبد الجليل <142967310+AliAbdJaleel@users.noreply.github.com>
2024-01-10 02:57:25 +0300

    add home data

--- settings/settings_context_processor.py
+++ settings/settings_context_processor.py
@@ -5,8 +5,9 @@ from django.core.cache import cache
 def get_settings(request):
 
 
-    settings_data = cache.get('settings_data')
-    if not settings_data:
-        settings_data = Settings.objects.last()
-        cache.set('settings_data',settings_data,60*60*24)
+    # settings_data = cache.get('settings_data')
+    # if not settings_data:
+    #     settings_data = Settings.objects.last()
+    #     cache.set('settings_data',settings_data,60*60*24)
+    settings_data = Settings.objects.last()
     return{'settings_data':settings_data}
