8b9e5d38c0cb8937641e378c9cf29918bba56497
علي قيس عبد الجليل <142967310+AliAbdJaleel@users.noreply.github.com>
2024-01-19 19:58:31 +0300

    add to cart

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-
-from .models import Order
+from django.shortcuts import render , redirect
+from products.models import Product
+from .models import Order , Cart , CartDetail
 
 def order_list(request):
     data = Order.objects.filter(user=request.user)
@@ -11,3 +11,16 @@ def order_list(request):
 def checkout(request):
 
     return render(request,'orders/checkout.html',{})
+
+
+def add_to_cart(request):
+    product = Product.objects.get(id=request.POST['product_id'])
+    quantity = int(request.POST['quantity'])
+    cart = Cart.objects.get(user=request.user,status='Inprogress')
+    cart_detail , created = CartDetail.objects.get_or_create(cart=cart,product=product)
+
+
+    cart_detail.quantity = quantity
+    cart_detail.total = round(product.price * cart_detail.quantity,2)
+    cart_detail.save()
+    return redirect(f'/products/{product.slug}')
