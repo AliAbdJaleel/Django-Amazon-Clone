1d3b8f82a04a936e7847e704afc49b423d75aced
علي قيس عبد الجليل <142967310+AliAbdJaleel@users.noreply.github.com>
2024-01-26 14:24:00 +0300

    show checkout page

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render , redirect
 from products.models import Product
 from .models import Order , Cart , CartDetail
+from settings.models import DeliveryFee
 
 def order_list(request):
     data = Order.objects.filter(user=request.user)
@@ -9,8 +10,19 @@ def order_list(request):
 
 
 def checkout(request):
-
-    return render(request,'orders/checkout.html',{})
+    cart = Cart.objects.get(user=request.user,status='Inprogress')
+    cart_detail=CartDetail.objects.filter(cart=cart)
+    delivery_fee = DeliveryFee.objects.last().fee
+    sub_total = cart.cart_total
+    discount = 0
+    total = sub_total + delivery_fee
+    return render(request,'orders/checkout.html',{
+        'cart_detail':cart_detail,
+        'delivery_fee': delivery_fee,
+        'sub_total':sub_total ,
+        'discount':discount,
+        'total':total
+    })
 
 
 def add_to_cart(request):
