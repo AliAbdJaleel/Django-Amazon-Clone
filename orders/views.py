157da87697b5bd36e93bbd8ea1849a4b9eb53e49
علي قيس عبد الجليل <142967310+AliAbdJaleel@users.noreply.github.com>
2024-01-26 20:33:59 +0300

    add  apply coupon

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render , redirect
+from django.shortcuts import get_object_or_404
+import datetime
 from products.models import Product
-from .models import Order , Cart , CartDetail
+from .models import Order , Cart , CartDetail , Coupon
 from settings.models import DeliveryFee
 
 def order_list(request):
@@ -10,19 +12,44 @@ def order_list(request):
 
 
 def checkout(request):
+   #pass
     cart = Cart.objects.get(user=request.user,status='Inprogress')
-    cart_detail=CartDetail.objects.filter(cart=cart)
+    cart_detail = CartDetail.objects.filter(cart=cart)
     delivery_fee = DeliveryFee.objects.last().fee
+    if request.method == 'POST':
+        code = request.POST['coupon_code']
+        #coupon = Coupon.objects.get(code=code)
+        coupon = get_object_or_404(Coupon,code=code)
+        if coupon and coupon.quantity > 0:
+            today_date = datetime.datetime.today().date()
+            if today_date >= coupon.start_date and today_date <= coupon.end_date:
+                coupon_value = round(cart.cart_total / 100*coupon.discount,2)
+                sub_total = cart.cart_total - coupon_value
+                total = sub_total + delivery_fee
+
+                cart.coupon = coupon
+                cart.total_with_coupon = sub_total
+                cart.save()
+                return render(request,'orders/checkout.html',{
+                    'cart_detail':cart_detail,
+                    'delivery_fee': delivery_fee,
+                    'sub_total':sub_total ,
+                    'discount':coupon_value,
+                    'total':total
+                 })
+
+
     sub_total = cart.cart_total
     discount = 0
     total = sub_total + delivery_fee
     return render(request,'orders/checkout.html',{
-        'cart_detail':cart_detail,
-        'delivery_fee': delivery_fee,
-        'sub_total':sub_total ,
-        'discount':discount,
-        'total':total
-    })
+                    'cart_detail':cart_detail,
+                    'delivery_fee': delivery_fee,
+                    'sub_total':sub_total ,
+                    'discount':discount,
+                    'total':total
+                 })
+   
 
 
 def add_to_cart(request):
